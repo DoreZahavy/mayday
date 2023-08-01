8cde5a0a072deceb11c30a96b322f9fe87eae719
Dore Zahavy <dorezahavy@gmail.com>
2023-08-01 16:15:47 +0300

    use remote mongo atlas db

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ import configProd from './prod.js'
 import configDev from './dev.js'
 
 export var config
-
+// config = configProd
 if (process.env.NODE_ENV === 'production') { //false && 
   config = configProd
 } else {
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// export default {
+//   dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+//   dbName : 'board_db'
+// }
+
 export default {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : 'board_db'
+  dbURL: process.env.ATLAS_URL,
+  dbName: process.env.ATLAS_DB_NAME,
 }
